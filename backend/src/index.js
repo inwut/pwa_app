4fb35c9efb295b08f71da6c92c3d83d08970953b
Daria Vetrykush <dariavetrykush@gmail.com>
2025-03-05 13:14:17 +0200

    initialize backend, add basic structure

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({status: "success", message: "api is running..."});
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
